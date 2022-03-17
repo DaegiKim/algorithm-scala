1576adc65d1b24cbfac0f84b6ea0da74a75fb95b
devin.kim <devin.kim@kakaocorp.com>
2022-03-17 13:28:43 +0900

    solve https://www.codewars.com/kata/5467e4d82edf8bbf40000155/solutions/scala

---
+++ src/main/scala/Order.scala
@@ -0,0 +1,9 @@
+object Order {
+  def descendingOrder(num: Int): Int = {
+    num
+      .toString
+      .sorted
+      .reverse
+      .toInt
+  }
+}
---
+++ src/test/scala/OrderTest.scala
@@ -0,0 +1,17 @@
+import org.scalatest._
+import flatspec._
+import matchers.should._
+
+class OrderTest extends AnyFlatSpec with Matchers {
+  "Order.desdendingOrder(0)" should "return 0" in {
+    Order.descendingOrder(0) should be (0)
+  }
+
+  "Order.desdendingOrder(51)" should "return 15" in {
+    Order.descendingOrder(15) should be (51)
+  }
+
+  "Order.desdendingOrder(123456789)" should "return 987654321" in {
+    Order.descendingOrder(123456789) should be (987654321)
+  }
+}
